e05fef49001b5e93e7ffd360819e3068a6f7b428
Mohd Shavez <88038219+MohdShavezz@users.noreply.github.com>
2024-10-21 18:51:43 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
+/**
+BASE_URL=http://localhost:3000
+BCRYPT_SALT=12
+JWT_KEY=DFASDFAGDFhdfgvdv
+MONGO_DB_URL=mongodb+srv://username:password>@cluster0.czbkt.mongodb.net/
+**/
 const nextConfig = {};
 
 module.exports = nextConfig;
